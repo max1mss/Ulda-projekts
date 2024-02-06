5f119880ec36fb7c5037d5fc976c60311168eefd
Maksims Jegorovs <mj2601@gymn.local>
2024-02-06 13:27:04 +0200

    komentars

---
+++ Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace uldis_ladite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_vards = new System.Windows.Forms.Label();
+            this.lb_uzvards = new System.Windows.Forms.Label();
+            this.lb_velt_teksts = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lb_platums = new System.Windows.Forms.Label();
+            this.lb_augstums = new System.Windows.Forms.Label();
+            this.lb_garums = new System.Windows.Forms.Label();
+            this.tb_vards = new System.Windows.Forms.TextBox();
+            this.tb_uzvards = new System.Windows.Forms.TextBox();
+            this.tb_velt_teksts = new System.Windows.Forms.TextBox();
+            this.tb_platums = new System.Windows.Forms.TextBox();
+            this.tb_augstums = new System.Windows.Forms.TextBox();
+            this.tb_garums = new System.Windows.Forms.TextBox();
+            this.tb_cena = new System.Windows.Forms.TextBox();
+            this.lb_cena = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lb_vards
+            // 
+            this.lb_vards.AutoSize = true;
+            this.lb_vards.Location = new System.Drawing.Point(105, 51);
+            this.lb_vards.Name = "lb_vards";
+            this.lb_vards.Size = new System.Drawing.Size(34, 13);
+            this.lb_vards.TabIndex = 0;
+            this.lb_vards.Text = "Vārds";
+            // 
+            // lb_uzvards
+            // 
+            this.lb_uzvards.AutoSize = true;
+            this.lb_uzvards.Location = new System.Drawing.Point(103, 85);
+            this.lb_uzvards.Name = "lb_uzvards";
+            this.lb_uzvards.Size = new System.Drawing.Size(46, 13);
+            this.lb_uzvards.TabIndex = 1;
+            this.lb_uzvards.Text = "Uzvārds";
+            // 
+            // lb_velt_teksts
+            // 
+            this.lb_velt_teksts.AutoSize = true;
+            this.lb_velt_teksts.Location = new System.Drawing.Point(67, 304);
+            this.lb_velt_teksts.Name = "lb_velt_teksts";
+            this.lb_velt_teksts.Size = new System.Drawing.Size(81, 13);
+            this.lb_velt_teksts.TabIndex = 2;
+            this.lb_velt_teksts.Text = "Veltījuma teksts";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(129, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Informācija par lādīti";
+            // 
+            // lb_platums
+            // 
+            this.lb_platums.AutoSize = true;
+            this.lb_platums.Location = new System.Drawing.Point(104, 193);
+            this.lb_platums.Name = "lb_platums";
+            this.lb_platums.Size = new System.Drawing.Size(44, 13);
+            this.lb_platums.TabIndex = 4;
+            this.lb_platums.Text = "Platums";
+            // 
+            // lb_augstums
+            // 
+            this.lb_augstums.AutoSize = true;
+            this.lb_augstums.Location = new System.Drawing.Point(103, 230);
+            this.lb_augstums.Name = "lb_augstums";
+            this.lb_augstums.Size = new System.Drawing.Size(53, 13);
+            this.lb_augstums.TabIndex = 5;
+            this.lb_augstums.Text = "Augstums";
+            // 
+            // lb_garums
+            // 
+            this.lb_garums.AutoSize = true;
+            this.lb_garums.Location = new System.Drawing.Point(105, 268);
+            this.lb_garums.Name = "lb_garums";
+            this.lb_garums.Size = new System.Drawing.Size(43, 13);
+            this.lb_garums.TabIndex = 6;
+            this.lb_garums.Text = "Garums";
+            // 
+            // tb_vards
+            // 
+            this.tb_vards.Location = new System.Drawing.Point(179, 51);
+            this.tb_vards.Name = "tb_vards";
+            this.tb_vards.Size = new System.Drawing.Size(100, 20);
+            this.tb_vards.TabIndex = 7;
+            // 
+            // tb_uzvards
+            // 
+            this.tb_uzvards.Location = new System.Drawing.Point(179, 85);
+            this.tb_uzvards.Name = "tb_uzvards";
+            this.tb_uzvards.Size = new System.Drawing.Size(100, 20);
+            this.tb_uzvards.TabIndex = 8;
+            // 
+            // tb_velt_teksts
+            // 
+            this.tb_velt_teksts.Location = new System.Drawing.Point(155, 304);
+            this.tb_velt_teksts.Name = "tb_velt_teksts";
+            this.tb_velt_teksts.Size = new System.Drawing.Size(100, 20);
+            this.tb_velt_teksts.TabIndex = 9;
+            // 
+            // tb_platums
+            // 
+            this.tb_platums.Location = new System.Drawing.Point(155, 193);
+            this.tb_platums.Name = "tb_platums";
+            this.tb_platums.Size = new System.Drawing.Size(100, 20);
+            this.tb_platums.TabIndex = 10;
+            // 
+            // tb_augstums
+            // 
+            this.tb_augstums.Location = new System.Drawing.Point(155, 230);
+            this.tb_augstums.Name = "tb_augstums";
+            this.tb_augstums.Size = new System.Drawing.Size(100, 20);
+            this.tb_augstums.TabIndex = 11;
+            // 
+            // tb_garums
+            // 
+            this.tb_garums.Location = new System.Drawing.Point(155, 268);
+            this.tb_garums.Name = "tb_garums";
+            this.tb_garums.Size = new System.Drawing.Size(100, 20);
+            this.tb_garums.TabIndex = 12;
+            // 
+            // tb_cena
+            // 
+            this.tb_cena.Location = new System.Drawing.Point(155, 345);
+            this.tb_cena.Name = "tb_cena";
+            this.tb_cena.Size = new System.Drawing.Size(100, 20);
+            this.tb_cena.TabIndex = 13;
+            // 
+            // lb_cena
+            // 
+            this.lb_cena.AutoSize = true;
+            this.lb_cena.Location = new System.Drawing.Point(32, 348);
+            this.lb_cena.Name = "lb_cena";
+            this.lb_cena.Size = new System.Drawing.Size(114, 13);
+            this.lb_cena.TabIndex = 14;
+            this.lb_cena.Text = "Materiāla cena eur/m2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(491, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Aprēķināt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(423, 345);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Saglabāt failā";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(423, 97);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(204, 242);
+            this.richTextBox1.TabIndex = 17;
+            this.richTextBox1.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(522, 345);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 23);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Saglabāt datubāzē";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lb_cena);
+            this.Controls.Add(this.tb_cena);
+            this.Controls.Add(this.tb_garums);
+            this.Controls.Add(this.tb_augstums);
+            this.Controls.Add(this.tb_platums);
+            this.Controls.Add(this.tb_velt_teksts);
+            this.Controls.Add(this.tb_uzvards);
+            this.Controls.Add(this.tb_vards);
+            this.Controls.Add(this.lb_garums);
+            this.Controls.Add(this.lb_augstums);
+            this.Controls.Add(this.lb_platums);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lb_velt_teksts);
+            this.Controls.Add(this.lb_uzvards);
+            this.Controls.Add(this.lb_vards);
+            this.Name = "Form1";
+            this.Text = "Programma priekš lādītes izveidošanas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_vards;
+        private System.Windows.Forms.Label lb_uzvards;
+        private System.Windows.Forms.Label lb_velt_teksts;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lb_platums;
+        private System.Windows.Forms.Label lb_augstums;
+        private System.Windows.Forms.Label lb_garums;
+        private System.Windows.Forms.TextBox tb_vards;
+        private System.Windows.Forms.TextBox tb_uzvards;
+        private System.Windows.Forms.TextBox tb_velt_teksts;
+        private System.Windows.Forms.TextBox tb_platums;
+        private System.Windows.Forms.TextBox tb_augstums;
+        private System.Windows.Forms.TextBox tb_garums;
+        private System.Windows.Forms.TextBox tb_cena;
+        private System.Windows.Forms.Label lb_cena;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.IO;
+using System.Reflection.Emit;
+
+namespace uldis_ladite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        int darba_samaksa = 15;
+        int PVN = 21;
+
+
+        static SQLiteConnection CreateConnection() // Programmas konnekcija ar SQL datubāzi
+        {
+            SQLiteConnection sqlite_conn;
+            sqlite_conn = new SQLiteConnection("Data Source=Ulda_ladite.db; Version=3; New=True; Compress=True;");
+            try
+            {
+                sqlite_conn.Open();
+            }
+            catch (Exception ex)
+            {
+                // Savienojuma kļūdu apstrāde
+            }
+            return sqlite_conn;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+            {
+            //Pārveido vēlējumu stringu uz int
+            string varda_ga = tb_velt_teksts.Text;
+            int gar = varda_ga.Length;
+
+            // Pārbauda vai visas nepieciešamās vērtības ir 
+            if (
+                int.TryParse(tb_platums.Text, out int platums) &&
+                int.TryParse(tb_garums.Text, out int garums) &&
+                int.TryParse(tb_augstums.Text, out int augstums) &&
+                int.TryParse(tb_cena.Text, out int materiala_cena))
+            {
+                //  saksaita characters tb_veltijums un reizina ar 1.2
+                string inputText = tb_velt_teksts.Text;
+                int charCount = inputText.Length;
+                double multipliedCount = charCount * 1.2;
+
+                // veic matemātiku
+                double produkta_cena = (gar * 1.2) + ((platums / 100) * (augstums / 100) * (garums / 100)) / 3 * materiala_cena;
+                double PVN_summa = (produkta_cena + darba_samaksa) * PVN / 100;
+                double rekina_summa = produkta_cena + darba_samaksa + PVN_summa;
+
+                // Parāda to RichTextBox
+                richTextBox1.Text = $"Produkta cena: {produkta_cena:C}\n" +
+                                   $"PVN summa: {PVN_summa:C}\n" +
+                                   $"Rekina summa: {rekina_summa:C}\n";
+
+
+
+          
+
+            }
+            else
+            {
+                // Kļūdas gadījumā programma izvada paziņojumu
+                richTextBox1.Text = "Iavadiet lūdzu pareizi.";
+            }
+
+
+
+            }
+
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            using (StreamWriter a = new StreamWriter("Cheks.txt"))
+            {
+                // Izveidojam jaunu mapi ar failu, kur saglabāsies informācija
+
+                a.WriteLine(lb_vards.Text + " " + tb_vards.Text);
+                a.WriteLine(lb_uzvards.Text + " " + tb_uzvards.Text);
+                a.WriteLine(lb_platums.Text + " " + tb_platums.Text);
+                a.WriteLine(lb_garums.Text + " " + tb_garums.Text);
+                a.WriteLine(lb_augstums.Text + " " + tb_augstums.Text);
+                a.WriteLine(lb_velt_teksts.Text + " " + tb_velt_teksts.Text);
+                a.WriteLine(lb_cena.Text + " " + tb_cena.Text);
+
+                
+                a.Close();
+                MessageBox.Show("Veiksmīgi saglabāts failā!");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (AreFieldsFilled())
+            {
+                SQLiteConnection sqlite_conn;
+                sqlite_conn = CreateConnection();
+
+                SQLiteCommand sqlite_cmd;
+                sqlite_cmd = sqlite_conn.CreateCommand();
+                sqlite_cmd.CommandText = "INSERT INTO Uldaizmaksas (Vards, Uzvards, Velejums, Laditesgarums, Laditesplatums, Laditesaugstums, Kokmaterialacena) " +
+                                         "VALUES('" + tb_vards.Text + "', '" + tb_uzvards.Text + "', '" +
+                                         tb_velt_teksts.Text + "', '" + tb_garums.Text + "', '" + tb_platums.Text + "', '"+tb_augstums.Text+"', '"+tb_cena.Text+"');";
+                sqlite_cmd.ExecuteNonQuery();
+
+                // Izvadām lodziņu, ja lietotājs ievadīja visu info.
+                MessageBox.Show("Jūs veiksmīgi saglabājāt informāciju datubāzē!");
+            }
+            else
+            {
+                MessageBox.Show("Lūdzu ievadiet visus datus!");
+            }
+        }
+        private bool AreFieldsFilled()
+        {
+            // Pārbaudām vai ir aizpildīti visi lodziņi
+            return !string.IsNullOrEmpty(tb_vards.Text) &&
+                   !string.IsNullOrEmpty(tb_uzvards.Text) &&
+                   !string.IsNullOrEmpty(tb_velt_teksts.Text) &&
+                   !string.IsNullOrEmpty(tb_garums.Text) &&
+                   !string.IsNullOrEmpty(tb_platums.Text) &&
+                   !string.IsNullOrEmpty(tb_augstums.Text) &&
+                   !string.IsNullOrEmpty(tb_cena.Text);
+        }
+    }
+}
